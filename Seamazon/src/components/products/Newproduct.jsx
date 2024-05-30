13144806c3a4ec91a992ffe2dc43d6d61ed91c0e
Kapish-patel <kpatelkapish@gmail.com>
2024-05-30 13:34:57 -0700

    final commit

--- Seamazon/src/components/products/Newproduct.jsx
+++ Seamazon/src/components/products/Newproduct.jsx
@@ -1,120 +1,174 @@
-import {useState} from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { addProduct } from '../../Redux/slice/productsSlice'
-import './Newproduct.css'
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addProduct } from "../../Redux/slice/productsSlice";
+import "./Newproduct.css";
 
 function Newproduct() {
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
 
     const user = useSelector((state) => state.user.userDetails);
 
     // state variables
-    const [productName, setProductName] = useState('')
-    const [productDescription, setProductDescription] = useState('')
-    const [productPrice, setProductPrice] = useState(0)
-    const [productQuantity, setProductQuantity] = useState(0)
-    const [productCategory, setProductCategory] = useState(1)
-    const [productImage, setProductImage] = useState('')
+    const [productName, setProductName] = useState("");
+    const [productDescription, setProductDescription] = useState("");
+    const [productPrice, setProductPrice] = useState(0);
+    const [productQuantity, setProductQuantity] = useState(0);
+    const [productCategory, setProductCategory] = useState(1);
+    const [productImage, setProductImage] = useState(null);
+    const [imagePreviewUrl, setImagePreviewUrl] = useState("");
 
     // handle save button click
+    const handleSavebtnClick = async () => {
+        // fetch the S3 URL to upload the image
+        const { url } = await fetch("/api/s3Url").then((res) => res.json());
 
-    const handleSavebtnClick = () => {
-        console.log('Product Name:', productName)
-        console.log('Product Description:', productDescription)
-        console.log('Product Price:', productPrice)
-        console.log('Product Quantity:', productQuantity)
-        console.log('Product Category:', productCategory)
-        console.log('Product Image:', productImage)
-        console.log('User:', user.user_id)
-        dispatch(addProduct({
-            name: productName,
-            description: productDescription,
-            price: productPrice,
-            quantity: productQuantity,
-            category: productCategory,
-            image: productImage,
-            userId: user.user_id
-        }))
+        await fetch(url, {
+            method: "PUT",
+            headers: {
+                "Content-Type": productImage.type,
+            },
+            body: productImage,
+        });
 
-        // clear the form 
-        handleResetbtnClick()
-    }   
+        const imageUrl = url.split("?")[0];
+        console.log(imageUrl);
+
+        console.log(
+            productName,
+            productDescription,
+            productPrice,
+            productQuantity,
+            productCategory,
+            imageUrl,
+            user.user_id
+        );
+
+        dispatch(
+            addProduct({
+                name: productName,
+                description: productDescription,
+                price: productPrice,
+                quantity: productQuantity,
+                category: productCategory,
+                image: imageUrl,
+                userId: user.user_id,
+            })
+        );
+        // clear the form
+        handleResetbtnClick();
+    };
 
     const handleResetbtnClick = () => {
-        setProductName('')
-        setProductDescription('')
-        setProductPrice(0)
-        setProductQuantity(0)
-        setProductCategory(1)
-        setProductImage('')
-    }
+        setProductName("");
+        setProductDescription("");
+        setProductPrice(0);
+        setProductQuantity(0);
+        setProductCategory(1);
+        setProductImage(null);
+        setImagePreviewUrl("");
+    };
 
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            setProductImage(file);
+            setImagePreviewUrl(URL.createObjectURL(file));
+        } else {
+            setProductImage(null);
+            setImagePreviewUrl("");
+        }
+    };
 
-  return (
-    <div className="main-content-container">
-      <div className="product-container">
-        <div className="product-header">
-          <p>New Product</p>
+    return (
+        <div className="main-content-container">
+            <div className="product-container">
+                <div className="product-header">
+                    <p>New Product</p>
+                </div>
+                <div className="product-body newproduct-body">
+                    <div className="info-section newproduct-info">
+                        <label>Product Name:</label>
+                        <input
+                            type="text"
+                            value={productName}
+                            onChange={(e) => {
+                                setProductName(e.target.value);
+                            }}
+                        />
+                    </div>
+                    <div className="info-section newproduct-info">
+                        <label>Product Description:</label>
+                        <textarea
+                            type="text"
+                            value={productDescription}
+                            onChange={(e) => {
+                                setProductDescription(e.target.value);
+                            }}
+                        />
+                    </div>
+                    <div className="info-section newproduct-info">
+                        <label>Product Price:</label>
+                        <input
+                            type="number"
+                            value={productPrice}
+                            onChange={(e) => {
+                                setProductPrice(Number(e.target.value));
+                            }}
+                        />
+                    </div>
+                    <div className="info-section newproduct-info">
+                        <label>Product Quantity:</label>
+                        <input
+                            type="number"
+                            value={productQuantity}
+                            onChange={(e) => {
+                                setProductQuantity(Number(e.target.value));
+                            }}
+                        />
+                    </div>
+                    <div className="info-section newproduct-info">
+                        <label>Product Category:</label>
+                        <select
+                            value={productCategory}
+                            onChange={(e) => {
+                                setProductCategory(Number(e.target.value));
+                            }}
+                        >
+                            <option value="1">
+                                Male Clothing & Accessories
+                            </option>
+                            <option value="2">
+                                Female Clothing & Accessories
+                            </option>
+                            <option value="3">Kids & Toys</option>
+                            <option value="5">Electronics & Technology</option>
+                            <option value="6">Home & Lifestyle</option>
+                            <option value="7">Pets & Animals</option>
+                            <option value="8">Health & Wellness</option>
+                            <option value="9">DIY & Industrial</option>
+                            <option value="10">Automotive & Vehicle</option>
+                        </select>
+                    </div>
+                    <div className="info-section newproduct-info">
+                        <label>Product Image:</label>
+                        <input type="file" onChange={handleImageChange} />
+                        {imagePreviewUrl && (
+                            <div className="image-preview">
+                                <img
+                                    src={imagePreviewUrl}
+                                    alt="Product Preview"
+                                />
+                            </div>
+                        )}
+                    </div>
+                    <div className="info-section newproduct-info">
+                        <button onClick={handleSavebtnClick}>Save</button>
+                        <button onClick={handleResetbtnClick}>Reset</button>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="product-body newproduct-body">
-            <div className="info-section newproduct-info">
-                <label> Product Name: </label>
-                <input type="text"
-                value={productName}
-                onChange={(e) => {setProductName(e.target.value)}}/>
-            </div>
-            <div className="info-section newproduct-info">
-                <label> Product Descrition: </label>
-                <textarea type="text"
-                value={productDescription}
-                onChange={(e) => {setProductDescription(e.target.value)}}/>
-            </div>
-            <div className="info-section newproduct-info">
-                <label> Product Price: </label>
-                <input type="decimal"
-                value={productPrice}
-                onChange={(e) => {setProductPrice(e.target.value)}}
-                />
-            </div>
-            <div className="info-section newproduct-info">
-                <label> Product Quantity: </label>
-                <input type="number"
-                value={productQuantity}
-                onChange={(e) => {setProductQuantity(e.target.value)}}
-                />
-            </div>
-            <div className="info-section newproduct-info">
-                <label> Product category:</label>
-                <select
-                value={productCategory}
-                onChange={(e) => {setProductCategory(e.target.value)}}
-                >
-                    <option value="Male Clothing & Accessories">Male Clothing & Accessories</option>
-                    <option value="Female Clothing & Accessories">Female Clothing & Accessories</option>
-                    <option value="Kids & Toys">Kids & Toys</option>
-                    <option value="Electronics & Technology">Electronics & Technology</option>
-                    <option value="Home & Lifestyle">Home & Lifestyle</option>
-                    <option value="Pets & Animals">Pets & Animals</option>
-                    <option value="Health & Wellness">Health & Wellness</option>
-                    <option value="DIY & Industrial">DIY & Industrial</option>
-                    <option value="Automotive & Vehicle">Automotive & Vehicle</option>
-                </select>
-            </div>
-            <div className="info-section newproduct-info">
-                <label> Product Image:</label>
-                <input type="file"
-                value={productImage}
-                onChange={(e) => {setProductImage(e.target.value)}}
-                />
-            </div>
-            <div className="info-section newproduct-info">
-                <button onClick={handleSavebtnClick}>Save</button>
-                <button onClick={handleResetbtnClick}>reset</button>
-            </div>
-        </div>
-      </div>
-    </div>
-  )
+    );
 }
 
-export default Newproduct
+export default Newproduct;
--- Seamazon/src/components/products/Product.jsx
+++ Seamazon/src/components/products/Product.jsx
@@ -5,16 +5,14 @@ import { CheckCircleOutline, Cancel } from "@mui/icons-material";
 import { Rating } from '@mui/material';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { getProduct } from '../../Redux/slice/productsSlice';
+import { getProduct, updateProduct } from '../../Redux/slice/productsSlice';
 import { useEffect, useState } from 'react';
-import { updateProduct } from '../../Redux/slice/productsSlice';
 
 function Product() {
     const dispatch = useDispatch();
     const { productId } = useParams();
 
     const productDetails = useSelector((state) => state.products.selectedProduct);
-    const productStatus = useSelector((state) => state.products.productStatus);
 
     const [isEditing, setIsEditing] = useState(false);
     const [description, setDescription] = useState('');
@@ -25,6 +23,8 @@ function Product() {
     const [stars, setStars] = useState(0);
     const [isBestSeller, setBestSeller] = useState(false);
     const [imgUrl, setImgUrl] = useState('');
+    const [newImage, setNewImage] = useState(null);
+    const [newImagePreviewUrl, setNewImagePreviewUrl] = useState('');
 
     useEffect(() => {
         if (productId) {
@@ -33,28 +33,62 @@ function Product() {
     }, [dispatch, productId]);
 
     useEffect(() => {
-        if (productStatus === 'Succeeded' && productDetails) {
-            setDescription(productDetails.description || '');
-            setPrice(productDetails.price || '');
-            setCategory(productDetails.category || '');
-            setQuantity(productDetails.quantity || '');
-            setTitle(productDetails.productName || '');
-            setStars(productDetails.ratings || 0);
-            setBestSeller(productDetails.bestSeller || false);
-            setImgUrl(productDetails.productImage || '');
+        if (productDetails) {
+            setDescription(productDetails.description);
+            setPrice(productDetails.price);
+            setCategory(productDetails.category);
+            setQuantity(productDetails.quantity);
+            setTitle(productDetails.productName);
+            setStars(productDetails.ratings);
+            setBestSeller(productDetails.bestSeller);
+            setImgUrl(productDetails.productImage);
         }
-    }, [productDetails, productStatus]);
+    }, [productDetails]);
 
-    const handleSaveBtn = () => {
-        const change = {
-                id: productId,
-                productName: title,
-                quantity: quantity,
-                price: price,
-                description: description,
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            setNewImage(file);
+            setNewImagePreviewUrl(URL.createObjectURL(file));
+        } else {
+            setNewImage(null);
+            setNewImagePreviewUrl('');
         }
-        dispatch(updateProduct(change))      
-        setIsEditing(false);  
+    };
+
+    const handleSaveBtn = async () => {
+        let imageUrl = imgUrl;
+
+        if (newImage) {
+            try {
+                // Fetch the S3 URL to upload the new image
+                const { url } = await fetch("/api/s3Url").then((res) => res.json());
+
+                await fetch(url, {
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": newImage.type,
+                    },
+                    body: newImage,
+                });
+
+                imageUrl = url.split("?")[0];
+            } catch (error) {
+                console.error("Error uploading image: ", error);
+            }
+        }
+
+        const change = {
+            id: productId,
+            productName: title,
+            quantity: quantity,
+            price: price,
+            description: description,
+            productImage: imageUrl,
+        };
+
+        dispatch(updateProduct(change));
+        setIsEditing(false);
     };
 
     return (
@@ -68,7 +102,7 @@ function Product() {
                 </div>
                 <div className="product-body product-content">
                     <div className="product-image">
-                        <img src={imgUrl} alt={title} />
+                        <img src={newImagePreviewUrl || imgUrl} alt={title} />
                     </div>
                     <div className="product-details product-page-details">
                         <div className="info-section">
@@ -121,7 +155,7 @@ function Product() {
                                 name={`rating-${productId}`}
                                 value={stars}
                                 precision={0.1}
-                                readOnly={!isEditing}
+                                readOnly
                             />
                         </div>
                         <div className="info-section">
@@ -133,11 +167,13 @@ function Product() {
                                 }
                             </div>
                         </div>
-                        <div className="info-section">
-                            {isEditing && 
+                        {isEditing && (
+                            <div className="info-section">
+                                <label>Change Product Image:</label>
+                                <input type="file" onChange={handleImageChange} />
                                 <button className="save-button" onClick={handleSaveBtn}>Save</button>
-                            }
-                        </div>
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
